1d787ac5901afff8ec7bcdec89803af5a884a1a3
Luis Ibanez <luis.ibanez@kitware.com>
2011-04-03 11:09:00 -0400

    ENH: Used awk to compute sum of vendors.

--- Data/MakeFigures.sh
+++ Data/MakeFigures.sh
@@ -1,3 +1,4 @@
 gnuplot LargestSoftwareVendorsPlot.gnuplot
 ps2pdf ../Art/LargestSoftwareVendorsPlot.ps ../Art/LargestSoftwareVendorsPlot.pdf
 rm ../Art/LargestSoftwareVendorsPlot.ps
+cat LargestSoftwareVendors.txt  | awk '{ SUM += $3} END { print SUM }'
