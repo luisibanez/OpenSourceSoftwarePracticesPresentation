dddcb02695e75906df23fae2dd48b7ba434ef52e
Luis Ibanez <luis.ibanez@kitware.com>
2011-04-05 13:01:28 -0400

    ENH: Added histogram of days to fix a bug.
    These are statistics from the ITK Mantis bug tracker.

--- Data/MakeFigures.sh
+++ Data/MakeFigures.sh
@@ -1,4 +1,7 @@
+gnuplot ITKBugFixesScheduleHistogramPlot.gnuplot
+ps2pdf ../Art/ITKBugFixesScheduleHistogramPlot.ps ../Art/ITKBugFixesScheduleHistogramPlot.pdf
+cat ITK-bugs-histogram.txt  | awk '{ SUM += $2} END { print "Number of Bugs = " SUM }'
 gnuplot LargestSoftwarePublishersPlot.gnuplot
 ps2pdf ../Art/LargestSoftwarePublishersPlot.ps ../Art/LargestSoftwarePublishersPlot.pdf
 rm ../Art/LargestSoftwarePublishersPlot.ps
-cat LargestSoftwarePublishers.txt  | awk '{ SUM += $3} END { print SUM }'
+cat LargestSoftwarePublishers.txt  | awk '{ SUM += $3} END { print "Size of Software Licensing industry = " SUM " Billions" }'
