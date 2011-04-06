bfc004dc75179f0650b2575574f971e6f72bafab
Luis Ibanez <luis.ibanez@kitware.com>
2011-04-06 03:17:21 -0400

    ENH: Added Hardware vendors.

--- Data/MakeFigures.sh
+++ Data/MakeFigures.sh
@@ -7,4 +7,7 @@ head -n 30 ITK-bugs-histogram.txt  | awk '{ SUM += $2} END { print "Number of Bu
 gnuplot LargestSoftwarePublishersPlot.gnuplot
 ps2pdf ../Art/LargestSoftwarePublishersPlot.ps ../Art/LargestSoftwarePublishersPlot.pdf
 rm ../Art/LargestSoftwarePublishersPlot.ps
+gnuplot LargestHardwareVendorsPlot.gnuplot
+ps2pdf ../Art/LargestHardwareVendorsPlot.ps ../Art/LargestHardwareVendorsPlot.pdf
 cat LargestSoftwarePublishers.txt  | awk '{ SUM += $3} END { print "Size of Software Licensing industry = " SUM " Billions" }'
+cat LargestHardwareVendors.txt  | awk '{ SUM += $3} END { print "Size of Hardware industry = " SUM " Billions" }'
