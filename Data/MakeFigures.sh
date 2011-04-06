434591e64fdf98bb5a9704247a59eb381f622d94
Luis Ibanez <luis.ibanez@kitware.com>
2011-04-06 00:47:09 -0400

    ENH: Computed bug percentiles ITK.

--- Data/MakeFigures.sh
+++ Data/MakeFigures.sh
@@ -1,6 +1,9 @@
 gnuplot ITKBugFixesScheduleHistogramPlot.gnuplot
 ps2pdf ../Art/ITKBugFixesScheduleHistogramPlot.ps ../Art/ITKBugFixesScheduleHistogramPlot.pdf
 cat ITK-bugs-histogram.txt  | awk '{ SUM += $2} END { print "Number of Bugs = " SUM }'
+head -n 7 ITK-bugs-histogram.txt  | awk '{ SUM += $2} END { print "Number of Bugs = " SUM " fixed in 1 week" }'
+head -n 14 ITK-bugs-histogram.txt  | awk '{ SUM += $2} END { print "Number of Bugs = " SUM " fixed in 2 weeks" }'
+head -n 30 ITK-bugs-histogram.txt  | awk '{ SUM += $2} END { print "Number of Bugs = " SUM " fixed in 1 month" }'
 gnuplot LargestSoftwarePublishersPlot.gnuplot
 ps2pdf ../Art/LargestSoftwarePublishersPlot.ps ../Art/LargestSoftwarePublishersPlot.pdf
 rm ../Art/LargestSoftwarePublishersPlot.ps
